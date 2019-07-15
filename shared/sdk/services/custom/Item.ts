c882df4a65cc9b968b48d7531f14d111d17bb608
jeremie <jeremieroland@orange.fr>
2019-07-15 22:14:51 +0200

    methode saveImageFile(file) dans ItemApi

--- shared/sdk/services/custom/Item.ts
+++ shared/sdk/services/custom/Item.ts
@@ -9,14 +9,22 @@ import { FileResponse } from '../../models/FileResponse';
 @Injectable()
 export class ItemApi extends BaseSailsApi {
 
-    private createWithImageFileUrl: string;
+    private createImageFileUrl: string;
 
     constructor(
         @Inject(HttpClient) protected http: HttpClient,
     ) {
         super(http, 'items');
-        this.createWithImageFileUrl = SailsConfig.getPath() + '/save-file';
+        this.createImageFileUrl = SailsConfig.getPath() + '/save-file';
     }
+
+    public saveImageFile(imageFile: File): Observable<FileResponse> {
+        let formData: FormData = new FormData();
+        formData.append('file', imageFile, imageFile.name);
+
+        return this.http.post<FileResponse>(this.createImageFileUrl, formData);
+    }
+
 
     public createWithImageFile(item: Item, imageFile: File): Observable<Item> {
         let formData: FormData = new FormData();
@@ -24,7 +32,7 @@ export class ItemApi extends BaseSailsApi {
 
         return new Observable(observer => {
 
-            this.http.post(this.createWithImageFileUrl, formData).subscribe( (fileResponse: FileResponse) => {
+            this.http.post(this.createImageFileUrl, formData).subscribe( (fileResponse: FileResponse) => {
                 item.image = fileResponse.fileName;
 
                 return this.create(item).subscribe( (storedItem: Item) => {
