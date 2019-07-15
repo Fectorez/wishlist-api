d0ba4ddf3604cf12b65fb16ca710fa0549351f77
jeremie <jeremieroland@orange.fr>
2019-07-15 20:31:10 +0200

    route & méthode save-file

---
+++ api/controllers/FileController.js
@@ -0,0 +1,32 @@
+/**
+ * ItemController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    saveFile: async function(req, res) {
+        const file = req.file('file');
+        let fileName;
+
+        file.upload({
+            dirname: '../../uploads/'
+        }, function(err, files) {
+            if (err) return res.serverError(err);
+            
+            let fileNameArray = files[0].fd.split("/");
+            fileName = fileNameArray[fileNameArray.length - 1];
+
+            res.json({
+                status: 201,
+                message: 'File uploaded successfully',
+                fileName: fileName
+            });
+        });
+
+        
+    }
+};
+
