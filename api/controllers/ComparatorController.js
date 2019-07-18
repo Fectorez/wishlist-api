749ba31b4ac0102a496fa2235133b34e5e0944a1
ItIsMagic <morabi.andrea@gmail.com>
2019-07-18 19:36:43 +0200

    comparator

---
+++ api/controllers/ComparatorController.js
@@ -0,0 +1,34 @@
+const amazonUrl = "https://www.amazon.fr/s?k=";
+const commerceUrl = "https://www.rueducommerce.fr/recherche/";
+const discountUrl = "https://www.cdiscount.com/search/10/";
+
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+module.exports = {
+
+  comparator: async function(req, res) {
+    var stringAmazon = req.query.string.replace(' ', '+');
+    var stringCommerce = req.query.string.replace(' ', '-');
+
+    const result = await axios.get(amazonUrl + stringAmazon);
+    const $ = cheerio.load(result.data);
+    
+    const item = $(".a-price-whole");
+    var priceAmazon = item.text().substring(0, item.text().indexOf(',') + 3);
+
+    const result2 = await axios.get(commerceUrl + stringCommerce);
+    const $2 = cheerio.load(result2.data);
+    
+    const item2 = $2(".price");
+    var priceCommerce = item2.text().substring(0, item2.text().indexOf('€') + 3);
+    priceCommerce = priceCommerce.replace('€', ',');
+
+    return res.send({
+          status: 200,
+          priceAmazon: priceAmazon,
+          priceCommerce: priceCommerce
+        });
+  }
+
+};
