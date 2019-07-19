9d451287a4a2f4dfae4bc39139e28d341879e40f
jeremie <jeremieroland@orange.fr>
2019-07-19 12:08:30 +0200

    + de méthodes

--- shared/sdk/services/custom/User.ts
+++ shared/sdk/services/custom/User.ts
@@ -23,24 +23,70 @@ export class UserApi extends BaseSailsApi {
         return this.http.get<Category[]>(this.actionUrl + id + '/' + this.categoriesRelation);
     }
 
-    public findByIdManagedPrizePools<PrizePool>(id: number): Observable<PrizePool[]> {
-        return this.http.get<PrizePool[]>(this.actionUrl + id + '/' + this.managedPrizePoolsRelation);
+    public updateByIdCategories(id: number, fk: number): Observable<User> {
+        return this.http.put<User>(this.actionUrl + id + '/' + this.categoriesRelation + fk, {});
     }
 
-    public findByIdDonations<Donation>(id: number): Observable<Donation[]> {
-        return this.http.get<Donation[]>(this.actionUrl + id + '/' + this.donationsRelation);
+    public updateByIdDeleteCategory(id: number, fk: number): Observable<User> {
+        return this.http.delete<User>(this.actionUrl + id + '/' + this.categoriesRelation + fk);
     }
 
-    public findByIdWishlists<Wishlist>(id: number): Observable<Wishlist[]> {
-        return this.http.get<Wishlist[]>(this.actionUrl + id + '/' + this.wishlistsRelation);
-    }
+
 
     public findByIdConcernedWishlists<Wishlist>(id: number): Observable<Wishlist[]> {
         return this.http.get<Wishlist[]>(this.actionUrl + id + '/' + this.concernedWishlistsRelation);
     }
 
-    public updateByIdCategories(id: number, fk: number): Observable<User> {
-        return this.http.put<User>(this.actionUrl + id + '/' + this.categoriesRelation + fk, {});
+    public updateByIdConcernedWishlists(id: number, fk: number): Observable<User> {
+        return this.http.put<User>(this.actionUrl + id + '/' + this.concernedWishlistsRelation + fk, {});
+    }
+
+    public updateByIdDeleteConcernedWishlist(id: number, fk: number): Observable<User> {
+        return this.http.delete<User>(this.actionUrl + id + '/' + this.concernedWishlistsRelation + fk);
+    }
+
+
+
+
+    public findByIdManagedPrizePools<PrizePool>(id: number): Observable<PrizePool[]> {
+        return this.http.get<PrizePool[]>(this.actionUrl + id + '/' + this.managedPrizePoolsRelation);
+    }
+
+    public updateByIdManagedPrizePools(id: number, fk: number): Observable<User> {
+        return this.http.put<User>(this.actionUrl + id + '/' + this.managedPrizePoolsRelation + fk, {});
+    }
+
+    public updateByIdDeleteManagedPrizePool(id: number, fk: number): Observable<User> {
+        return this.http.delete<User>(this.actionUrl + id + '/' + this.managedPrizePoolsRelation + fk);
+    }
+
+
+
+    public findByIdDonations<Donation>(id: number): Observable<Donation[]> {
+        return this.http.get<Donation[]>(this.actionUrl + id + '/' + this.donationsRelation);
+    }
+
+    public updateByIdDonations(id: number, fk: number): Observable<User> {
+        return this.http.put<User>(this.actionUrl + id + '/' + this.donationsRelation + fk, {});
+    }
+
+    public updateByIdDeleteDonation(id: number, fk: number): Observable<User> {
+        return this.http.delete<User>(this.actionUrl + id + '/' + this.donationsRelation + fk);
+    }
+
+
+
+
+    public findByIdWishlists<Wishlist>(id: number): Observable<Wishlist[]> {
+        return this.http.get<Wishlist[]>(this.actionUrl + id + '/' + this.wishlistsRelation);
+    }
+
+    public updateByIdWishlists(id: number, fk: number): Observable<User> {
+        return this.http.put<User>(this.actionUrl + id + '/' + this.wishlistsRelation + fk, {});
+    }
+
+    public updateByIdDeleteWishlist(id: number, fk: number): Observable<User> {
+        return this.http.delete<User>(this.actionUrl + id + '/' + this.wishlistsRelation + fk);
     }
 
     public createWishlist<Wishlist>(id: number, data: Wishlist): Observable<Wishlist> {
